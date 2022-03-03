c8955d0150ac791a2debf4065b213682423f395b
Ekici-Burak <b.ekici@it-students.fr>
2022-03-03 18:44:24 +0100

    npm i @vee-validate/i18n pour changer les message derreur

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -18,12 +18,14 @@
 
         <div class="form-group">
           <label for="email">Email</label>
-          <VeeField name="email" v-model="form.email" id="email" type="email" class="form-input" />
+          <VeeField name="email" v-model="form.email" id="email" type="email" class="form-input" rules="required|email" />
+          <VeeErrorMessage name="email" class="form-error" />
         </div>
 
         <div class="form-group">
           <label for="password">Password</label>
-          <VeeField name="password" v-model="form.password" id="password" type="password" class="form-input" />
+          <VeeField name="password" v-model="form.password" id="password" type="password" class="form-input" rules="required|min:8" />
+          <VeeErrorMessage name="password" class="form-error" />
         </div>
 
         <div class="form-group">
--- src/plugins/VeeValidatePlugin.js
+++ src/plugins/VeeValidatePlugin.js
@@ -1,11 +1,12 @@
 import {defineRule , Form, Field , ErrorMessage} from 'vee-validate'
+import {required, email, min, max } from '@vee-validate/rules'
 
 export default (app)=>{
 
-  defineRule('required', (value)=>{
-    if(value && value.trim()) return true
-    return 'this is required'
-  })                                //defineRule permet de definir un rule globalement, on peux l'utiliser dans les component , le premeir argument est le nom de la rule, le second => la function
+  defineRule('required', required )                                //defineRule permet de definir un rule globalement, on peux l'utiliser dans les component , le premeir argument est le nom de la rule, le second => la function
+  defineRule('email', email )                                //defineRule permet de definir un rule globalement, on peux l'utiliser dans les component , le premeir argument est le nom de la rule, le second => la function
+  defineRule('min', min )                                //defineRule permet de definir un rule globalement, on peux l'utiliser dans les component , le premeir argument est le nom de la rule, le second => la function
+  defineRule('max', max )                                //defineRule permet de definir un rule globalement, on peux l'utiliser dans les component , le premeir argument est le nom de la rule, le second => la function
 
   app.component('VeeForm' , Form)
   app.component('VeeField' , Field)
